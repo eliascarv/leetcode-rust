826f9dc4bde8b986618cd58f5ab261bffb1224d9
Elias Carvalho <eliascarvdev@gmail.com>
2023-05-22 23:01:22 -0300

    Inital commit

---
+++ src/solution/mod.rs
@@ -0,0 +1,1 @@
+pub mod s0001_two_sum;
---
+++ src/solution/s0001_two_sum.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+
+pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<u32> {
+    for i in 0..nums.len() {
+        for j in (i + 1)..nums.len() {
+            if nums[i] + nums[j] == target {
+                return vec![i as u32, j as u32];
+            }
+        }
+    }
+    vec![]
+}
+
+pub fn two_sum2(nums: Vec<i32>, target: i32) -> Vec<u32> {
+    let mut map = HashMap::new();
+    for (i, num) in nums.iter().enumerate() {
+        let diff = target - num;
+        match map.get(&diff) {
+            None => { 
+                map.insert(num, i as u32); 
+            }
+            Some(val) => {
+                return vec![*val, i as u32];
+            }
+        }
+    }
+    vec![]
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_v1() {
+        let nums = vec![2, 7, 11, 15]; 
+        let target = 9;
+        let result = two_sum(nums, target);
+        assert_eq!(result, vec![0, 1]);
+    }
+
+    #[test]
+    fn test_v2() {
+        let nums = vec![2, 7, 11, 15]; 
+        let target = 9;
+        let result = two_sum2(nums, target);
+        assert_eq!(result, vec![0, 1]);
+    }
+}
