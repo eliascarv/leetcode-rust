544808a99f625a035ee94674f0c84720def09c24
Elias Carvalho <eliascarvdev@gmail.com>
2023-05-28 09:45:08 -0300

    Add single_number v2

--- src/solution/s0136_single_number.rs
+++ src/solution/s0136_single_number.rs
@@ -13,6 +13,10 @@ pub fn single_number1(nums: &[i32]) -> i32 {
     seen[0]
 }
 
+pub fn single_number2(nums: &[i32]) -> i32 {
+    nums.iter().fold(0, |acc, x| acc ^ x)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -25,5 +29,15 @@ mod tests {
         assert_eq!(single_number1(&a), 1);
         assert_eq!(single_number1(&b), 4);
         assert_eq!(single_number1(&c), 1);
+    }
+
+    #[test]
+    fn test_single_number_v2() {
+        let a = vec![2, 2, 1];
+        let b = vec![4, 1, 2, 1, 2];
+        let c = vec![1];
+        assert_eq!(single_number2(&a), 1);
+        assert_eq!(single_number2(&b), 4);
+        assert_eq!(single_number2(&c), 1);
     }
 }
