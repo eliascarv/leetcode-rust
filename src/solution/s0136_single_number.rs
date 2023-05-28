ccf709a15ef54eec05822acea3e166ca62922cc9
Elias Carvalho <eliascarvdev@gmail.com>
2023-05-28 11:10:31 -0300

    Add single_number v3

--- src/solution/s0136_single_number.rs
+++ src/solution/s0136_single_number.rs
@@ -14,6 +14,14 @@ pub fn single_number1(nums: &[i32]) -> i32 {
 }
 
 pub fn single_number2(nums: &[i32]) -> i32 {
+    let mut single = 0;
+    for num in nums {
+        single ^= num;
+    }
+    single
+}
+
+pub fn single_number3(nums: &[i32]) -> i32 {
     nums.iter().fold(0, |acc, x| acc ^ x)
 }
 
@@ -39,5 +47,15 @@ mod tests {
         assert_eq!(single_number2(&a), 1);
         assert_eq!(single_number2(&b), 4);
         assert_eq!(single_number2(&c), 1);
+    }
+
+    #[test]
+    fn test_single_number_v3() {
+        let a = vec![2, 2, 1];
+        let b = vec![4, 1, 2, 1, 2];
+        let c = vec![1];
+        assert_eq!(single_number3(&a), 1);
+        assert_eq!(single_number3(&b), 4);
+        assert_eq!(single_number3(&c), 1);
     }
 }
