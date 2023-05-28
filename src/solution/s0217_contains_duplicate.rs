f6da977abd7f6ba446be6622ea0d641623a0ff92
Elias Carvalho <eliascarvdev@gmail.com>
2023-05-27 22:51:30 -0300

    Add contains_duplicate3

--- src/solution/s0217_contains_duplicate.rs
+++ src/solution/s0217_contains_duplicate.rs
@@ -13,8 +13,13 @@ pub fn contains_duplicate1(nums: &[i32]) -> bool {
 }
 
 pub fn contains_duplicate2(nums: &[i32]) -> bool {
-    let set = HashSet::<_>::from_iter(nums);
+    let set: HashSet<_> = nums.iter().collect();
     nums.len() > set.len()
+}
+
+pub fn contains_duplicate3(nums: &[i32]) -> bool {
+    let mut set = HashSet::new();
+    nums.iter().any(|num| !set.insert(num))
 }
 
 #[cfg(test)]
@@ -41,5 +46,16 @@ mod test {
         assert_eq!(contains_duplicate2(&a), true);
         assert_eq!(contains_duplicate2(&b), false);
         assert_eq!(contains_duplicate2(&c), true);
+    }
+
+    #[test]
+    fn test_contains_duplicate_v3() {
+        let a = vec![1, 2, 3, 1];
+        let b = vec![1, 2, 3, 4];
+        let c = vec![1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
+
+        assert_eq!(contains_duplicate3(&a), true);
+        assert_eq!(contains_duplicate3(&b), false);
+        assert_eq!(contains_duplicate3(&c), true);
     }
 }
