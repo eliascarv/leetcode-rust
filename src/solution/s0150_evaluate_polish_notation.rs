5f61154db767da34390eecf888be5daa14ae50e1
Elias Carvalho <eliascarvdev@gmail.com>
2023-06-29 18:25:53 -0300

    Add eval_rpn v2

--- src/solution/s0150_evaluate_polish_notation.rs
+++ src/solution/s0150_evaluate_polish_notation.rs
@@ -1,7 +1,7 @@
 // Problem 150. Evaluate Reverse Polish Notation
 // https://leetcode.com/problems/evaluate-reverse-polish-notation/
 
-pub fn eval_rpn(tokens: Vec<&str>) -> i32 {
+pub fn eval_rpn1(tokens: Vec<&str>) -> i32 {
     let mut filo: Vec<i32> = Vec::new();
 
     for token in tokens {
@@ -15,7 +15,7 @@ pub fn eval_rpn(tokens: Vec<&str>) -> i32 {
                 "-" => filo.push(left - right),
                 "*" => filo.push(left * right),
                 "/" => filo.push(left / right),
-                _   => unreachable!(),
+                _ => unreachable!(),
             };
         }
     }
@@ -23,19 +23,80 @@ pub fn eval_rpn(tokens: Vec<&str>) -> i32 {
     filo.pop().unwrap()
 }
 
+enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Op {
+    fn from_str(token: &str) -> Option<Op> {
+        match token {
+            "+" => Some(Op::Add),
+            "-" => Some(Op::Sub),
+            "*" => Some(Op::Mul),
+            "/" => Some(Op::Div),
+            _ => None,
+        }
+    }
+
+    fn apply(self, left: i32, right: i32) -> i32 {
+        match self {
+            Op::Add => left + right,
+            Op::Sub => left - right,
+            Op::Mul => left * right,
+            Op::Div => left / right,
+        }
+    }
+}
+
+pub fn eval_rpn2(tokens: Vec<&str>) -> Option<i32> {
+    let mut filo = Vec::new();
+
+    for token in tokens {
+        if let Some(op) = Op::from_str(token) {
+            let right = filo.pop()?;
+            let left = filo.pop()?;
+            filo.push(op.apply(left, right))
+        } else {
+            let num = token.parse().expect("integer number");
+            filo.push(num);
+        }
+    }
+
+    filo.pop()
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn test_eval_rpn() {
-        let tokens: Vec<&str> = vec!["2","1","+","3","*"];
-        assert_eq!(eval_rpn(tokens), 9);
+    fn test_eval_rpn1() {
+        let tokens: Vec<&str> = vec!["2", "1", "+", "3", "*"];
+        assert_eq!(eval_rpn1(tokens), 9);
 
-        let tokens: Vec<&str> = vec!["4","13","5","/","+"];
-        assert_eq!(eval_rpn(tokens), 6);
+        let tokens: Vec<&str> = vec!["4", "13", "5", "/", "+"];
+        assert_eq!(eval_rpn1(tokens), 6);
 
-        let tokens: Vec<&str> = vec!["10","6","9","3","+","-11","*","/","*","17","+","5","+"];
-        assert_eq!(eval_rpn(tokens), 22);
+        let tokens: Vec<&str> = vec![
+            "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+",
+        ];
+        assert_eq!(eval_rpn1(tokens), 22);
+    }
+
+    #[test]
+    fn test_eval_rpn2() {
+        let tokens = vec!["2", "1", "+", "3", "*"];
+        assert_eq!(eval_rpn2(tokens), Some(9));
+
+        let tokens = vec!["4", "13", "5", "/", "+"];
+        assert_eq!(eval_rpn2(tokens), Some(6));
+
+        let tokens = vec![
+            "10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+",
+        ];
+        assert_eq!(eval_rpn2(tokens), Some(22));
     }
 }
